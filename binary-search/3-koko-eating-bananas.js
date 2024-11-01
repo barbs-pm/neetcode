1141575b990a3c7bb645a1b6d3943f1589c8d47e
barbs <barbarapmarkus@gmail.com>
2024-11-01 16:10:43 -0300

    :sparkles: koko eating bananas solving

---
+++ binary-search/3-koko-eating-bananas.js
@@ -0,0 +1,30 @@
+// koko tem q comer a menor qnt possivel de banana em H horas
+// usar busca binaria pra encontrar K
+// eh um valor entre 1 e o valor maximo das pilhas,
+// ao inves de tentar 1 por um, da pra usar busca binaria pra achar o valor
+function minEatingSpeed(piles, h) {
+    let l = 1;
+    let r = Math.max(...piles);
+    let res = r;
+
+    while (l <= r) {
+        const k = Math.floor((l + r) / 2);
+
+        let totalTime = 0;
+        for (const p of piles) {
+            totalTime += Math.ceil(p / k);
+        }
+        if (totalTime <= h) {
+            res = k;
+            r = k - 1;
+        } else {
+            l = k + 1;
+        }
+    }
+    return res;
+}
+
+// 1 b/h = 10 horas > 9
+// 2 b/h = 6 horas => 1/2=1 + 4/2=2 + 3/2=1 + 2/2=2
+console.log(minEatingSpeed(piles = [1,4,3,2], h = 9)); // 2
+console.log(minEatingSpeed(piles = [25,10,23,4], h = 4)); // 25
