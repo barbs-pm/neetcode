ba160b53e81f2a48f5b895785a4ea01feb668287
barbs <barbarapmarkus@gmail.com>
2024-11-06 19:28:54 -0300

    :sparkles: add mergeKLists function for merging k sorted arrays

---
+++ linked-list/10-merge-k-sorted-arrays.js
@@ -0,0 +1,36 @@
+function mergeKLists(lists) {
+    if (!lists || lists.length === 0) {
+        return null;
+    }
+    return divide(lists, 0, lists.length - 1);
+}
+
+function divide(lists, l, r ) {
+    if (l > r) {
+        return null;
+    }
+    if (l === r) {
+        return lists[l]
+    }
+    const mid = Math.floor(l + (r - l) / 2);
+    const left = divide(lists, l, mid);
+    const right = divide(lists, mid + 1, r);
+    return conquer(left, right);
+}
+
+function conquer(l, r) {
+    const dummy = new ListNode(0);
+    let curr = dummy;
+    while (l && r) {
+        if (l.val <= r.val) {
+            curr.next = l;
+            l = l.next;
+        } else {
+            curr.next = r;
+            r = r.next;
+        }
+        curr = curr.next;
+    }
+    curr.next = l1 ? l1 : l2;
+    return dummy.next;
+}
